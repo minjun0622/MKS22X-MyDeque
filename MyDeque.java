00624a10e7fad320c6d4e60cb6ab50edef36ee2f
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-03-22 14:38:02 -0400

    Filled in the lab with things.

--- MyDeque.java
+++ MyDeque.java
@@ -9,6 +9,7 @@ public class MyDeque<E>{
   */
 
   public MyDeque(){
+    @SuppressWarnings("unchecked")
     E[] e = (E[])new Object[10];
     data = e;
     size = data.length;
@@ -16,7 +17,8 @@ public class MyDeque<E>{
   }
 
   public MyDeque(int initialCapacity) {
-    E[] e = new Object[initialCapacity];
+    @SuppressWarnings("unchecked")
+      E[] e = (E[])new Object[initialCapacity];
     data = e;
     size = data.length;
     end = initialCapacity;
@@ -27,11 +29,15 @@ public class MyDeque<E>{
     return size;
   }
 
+
+  //toString O(n) - format:  {a b c d }  / {}  /   {VALUE_VALUE2_VALUE3_}  (space after each value)
+
   public String toString(){
     String result = "{";
     for (int i = 0; i < data.length; i++) {
-      result += data[i] + " }";
+      result += data[i] + " ";
     }
+    result += "}";
     return result;
   }
 
@@ -44,33 +50,46 @@ public class MyDeque<E>{
     }
   }
 
+// {3, 4, 2, 1, 6} if I wanted to add. Increase size.
+//make a new array and copy it over. I multiplied the size by 2 but dk if it's right. I then add.
   public void addLast(E element){
-    data[end - 1] = element;
+    E[] temp = new E[size * 2];
+    for (int i = 0; i < data.length; i++){
+      temp[i] = data[i];
+    }
+    temp[data.length] = element;
   }
 
   public E removeFirst(){
-    for (int i = data.length - 1; i > 0; i--){
-      if (i > start) {
-        data[i - 1] = data[i];
-      }
-      else {
-        return data[i];
-      }
+    E temp = data[start];
+    E[] newdata = new E[size - 1];
+    for (int i = 1; i < data.length + 1; i++){
+      newdata[i - 1] = data[i];
+      data[i - 1] = newdata[i - 1];
     }
+
+    return temp;
   }
 
   public E removeLast(){
-
+    E temp = data[end];
+    for (int i = 0; i < data.length; i++){
+      E[] newdata = new E[size * 2];
+    }
+    return temp;
   }
-  
+
+//{e1, e2, e3, e4}, returns e
   public E getFirst(){
-
+    return data[start];
   }
-  public E getLast(){
 
+  public E getLast(){
+    return data[end];
   }
 
 public static void main(String[] args) {
+
 
 }
 
