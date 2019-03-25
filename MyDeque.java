95cc8a7f5172acb9ab31f1fbd3b9781c10798e1c
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-03-25 01:18:01 -0400

    got a toString fixed, adding other changes.

--- MyDeque.java
+++ MyDeque.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
@@ -26,77 +27,136 @@ public class MyDeque<E>{
     start = 0;
   }
 
+//can't just return. make sure it is within the start to end of array.
   public int size(){
-    return size;
+    if (end >= start) {
+      return end - start;
+    }
+    return (size - start) + end - 1;
   }
 
   //toString O(n) - format:  {a b c d }  / {}  /   {VALUE_VALUE2_VALUE3_}  (space after each value)
 
   public String toString(){
     String result = "{";
-    for (int i = 0; i < data.length; i++) {
-      result += data[i] + " ";
+    if (start == end){
+      return "{}";
+    }
+    //from start to end.
+    if (end > start){
+      for (int i = start; i < end; i++){
+        result += data[i] + " ";
+      }
+    }
+    else {
+      //if start is greater than end.
+      for (int i = start; i < size; i++){
+        result += data[i] + " ";
+      }
+      for (int i=0; i < end; i++){
+        result += data[i] + " ";
+      }
     }
     result += "}";
     return result;
   }
 
-  private void resize() {
-    @SuppressWarnings("unchecked")
-    E[] temp = (E[]) new Object[size * 2 + 1];
-    for (int i = 0; i < size; i++){
-      temp[i] = data[i];
-    }
-    size = size * 2 + 1
-  }
+
+  private void resize(){
+   @SuppressWarnings("unchecked")
+   E[] newData = (E[])new Object[size * 2];
+   int count = 0;
+   if (end >= start){
+     for (int i = start; i < end; i++){
+       newData[count] = data[i];
+       count++;
+     }
+   } else {
+     for (int i=start; i<size; i++){
+       newData[count] = data[i];
+       count++;
+     }
+     for (int i=0; i<end; i++){
+       newData[count] = data[i];
+       count++;
+     }
+   }
+   start = 0;
+   end = size();
+   size = size * 2;
+   data = newData;
+}
 
   public void addFirst(E element){
-    resize();
-    if (end == data.length - 1) {
-        end = 0;
+    if (size() == 0){
+      end++;
+      data[end-1] = element;
+    }
+    else {
+      if (end == size || end == start){
+        resize();
       }
-      else {
+      if (start == 0){
+        for (int i=end; i>=1; i--){
+          data[i] = data[i-1];
+        }
+        data[0] = null;
+        start++;
         end++;
       }
-      data[end] = element;
-      size++;
+      start--;
+      data[start] = element;
+    }
   }
 
 // {3, 4, 2, 1, 6} if I wanted to add. Increase size.
 //make a new array and copy it over. I multiplied the size by 2 but dk if it's right. I then add.
   public void addLast(E element){
-    resize();
-    if (end == data.length - 1) {
-    end = 0;
-  }
-  else {
+    if (size() == size) {
+      resize();
+    }
+    if (end == size){
+    data[0] = element;
+    end = 1;
+  } else {
+    data[end] = element;
     end++;
   }
-  data[end] = element;
-  size++;
-  }
+}
 
   public E removeFirst(){
-    int temp = start;
-    start++;
-    size++;
-    return data[temp];
+    E temp = data[start];
+    data[start] = null;
+    if (start == end - 1){
+      start = 0;
+      end = 0;
+      return temp;
+    }
+    if (start < size - 1){
+      start++;
+    }
+    else {
+      start = 0;
+    }
+    return temp;
   }
 
   public E removeLast(){
-    int temp = end;
+    E temp = data[end - 1];
     end--;
-    size--;
-    return data[temp];
+    return temp;
   }
 
 //{e1, e2, e3, e4}, returns e
   public E getFirst(){
+    if (start == end){
+    throw new NoSuchElementException();
+  }
     return data[start];
   }
 
   public E getLast(){
-    return data[end];
+    return data[end - 1];
   }
 
 public static void main(String[] args) {
